86e9131699282ecd556f9610945f3d62d6b5c572
GitHubAkanksha <33241906+GitHubAkanksha@users.noreply.github.com>
2018-02-14 09:35:37 +0800

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,26 @@
+// JavaScript source code
+
+"use strict";
+
+var http = require('http');
+
+http.get('http://www.omdbapi.com/?apikey=44269ab5&t=frozen', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+	alert('Inside the function!');
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+
+
